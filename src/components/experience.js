d257a1ca69d90c4f3e324c15e8993e2a6917927a
Darin Levesque <darin@darinlevesque.com>
2021-03-16 19:59:51 -0400

    serverless and dark mode testing

--- src/components/experience.js
+++ src/components/experience.js
@@ -16,8 +16,8 @@ const Experience = ({ data }) => (
       ))}
   </section> */}
   <section class="text-gray-600 body-font overflow-hidden">
+  <h1 className="section-header">Experience</h1>
   <div class="container px-5 py-24 mx-auto">
-    <h1 className="section-header">Experience</h1>
     <div class="-my-8 divide-y-2 divide-gray-100">
     {data &&
       data.map((item, i) => (
@@ -29,7 +29,7 @@ const Experience = ({ data }) => (
         <div class="md:flex-grow">
           <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">{item.role}</h2>
           <p class="leading-relaxed">{item.description}</p>
-          <a class="text-indigo-500 inline-flex items-center mt-4">Learn More
+          <a href="#" class="text-indigo-500 inline-flex items-center mt-4">Learn More
             <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
               <path d="M5 12h14"></path>
               <path d="M12 5l7 7-7 7"></path>
